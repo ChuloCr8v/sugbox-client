96daffc461d3f9347a5c31f0dca0509bf0bfc675
ChuloCr8v <chulocr8v@gmail.com>
2025-02-24 03:42:33 +0100

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-// export const API_URL = "https://sug-server-gf7y.onrender.com";
-export const API_URL = "http://localhost:8000/api/";
+export const API_URL = "https://sug-server-gf7y.onrender.com";
+// export const API_URL = "http://localhost:8000/api/";
 
 export const AUTH_STORAGE_KEY = "sugbox_authKey";
 export const TOKEN_STORAGE_KEY = "sugbox_token";
